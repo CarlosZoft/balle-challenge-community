b4ff67099e8e4e2a2c2f1a0a7f8699855b3ca4e8
Carlos Zoft <carlosk2gunn@hotmail.com>
2022-02-27 03:00:36 -0300

    🐛 fix: pipeline build error

--- src/controller/interface/IControllerHandler.ts
+++ src/controller/interface/IControllerHandler.ts
@@ -1,6 +1,4 @@
 import { Response, Request } from 'express';
-import { IRequest } from '../../interfaces/IHtpp';
-
 export interface ControllerHandler {
     handle(request: Request, response: Response): Promise<void>;
 }
